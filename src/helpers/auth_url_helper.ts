e1aa3e8289675bc191cce4ad30b6fa3f23342b1e
Netia Paul <netiapaul23@gmail.com>
2026-02-20 11:57:24 +0300

    - added the auth APIs

--- src/helpers/auth_url_helper.ts
+++ src/helpers/auth_url_helper.ts
@@ -1,3 +1,12 @@
 // LOgin
 export const POST_LOGIN = "/Auth";
 export const POST_LOGOUT = "/Auth/Logout";
+export const AUTH_BRANCH = "/Branches/ValidateBranch";
+// Client Modules
+export const GET_CLIENT_MODULES = "/Branches/GetClientModules";
+// STOCK EXPIRY
+export const EXPIRED_STOCKS = "/api/Dashboard/StockExpiry";
+// TILLS LIST
+export const AVAILABLE_TILLS = "/api/Tills/GetAvailableTills";
+// SYSDEFAULTS
+export const SYSDEFAULTS = "/SysDefaults";
