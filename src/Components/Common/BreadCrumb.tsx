7af3d32320d12f4e2199a0ebc922347fb094a9a3
Netia Paul <netiapaul23@gmail.com>
2026-02-20 11:54:00 +0300

    - added breadcramb to show the active component and licensed user and loggedin user

--- src/Components/Common/BreadCrumb.tsx
+++ src/Components/Common/BreadCrumb.tsx
@@ -1,32 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Col, Row } from 'reactstrap';
+import React from "react";
+// import { Link } from "react-router-dom";
+import { Col, Row } from "reactstrap";
 
 interface BreadCrumbProps {
-    title: string;
-    pageTitle : string;
+  title: string;
+  pageTitle: string;
 }
 
-const BreadCrumb = ({ title, pageTitle } : BreadCrumbProps) => {
-    return (
-        <React.Fragment>
-            <Row>
-                <Col xs={12}>
-                    <div className="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 className="mb-sm-0">{title}</h4>
+const BreadCrumb = ({ title, pageTitle }: BreadCrumbProps) => {
+  const authUser: any = localStorage.getItem("authUser");
+  const allCompanyUsers = JSON.parse(authUser)["user"].allCompanyUsers || null;
+  const loggedInUsers = JSON.parse(authUser)["user"].loggedInUsers || null;
 
-                        <div className="page-title-right">
-                            <ol className="breadcrumb m-0">
-                                <li className="breadcrumb-item"><Link to="#">{pageTitle}</Link></li>
-                                <li className="breadcrumb-item active">{title}</li>
-                            </ol>
-                        </div>
+  return (
+    <React.Fragment>
+      <Row>
+        <Col xs={12}>
+          <div className="page-title-box d-sm-flex align-items-center justify-content-between">
+            <div className="page-title-right">
+              <ol className="breadcrumb m-0">
+                <li className="breadcrumb-item">{pageTitle}</li>
+                <li className="breadcrumb-item active">{title}</li>
+              </ol>
+            </div>
 
-                    </div>
-                </Col>
-            </Row>
-        </React.Fragment>
-    );
+            <h6 className="mb-sm-0">
+              <span className="text-primary">
+                Licensed Users:{" "}
+                <span className="fw-bold text-danger">{allCompanyUsers}</span>
+              </span>{" "}
+              |{" "}
+              <span className="text-primary">
+                {/* <Link
+                  to="/users-setup/loggedin-users"
+                  role="button"
+                  className="text-decoration-none"
+                >
+                  Loggedin Users:{" "}
+                </Link> */}
+                Loggedin Users:{" "}
+                <span className="fw-bold text-danger">{loggedInUsers}</span>
+              </span>
+            </h6>
+          </div>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 };
 
 export default BreadCrumb;
